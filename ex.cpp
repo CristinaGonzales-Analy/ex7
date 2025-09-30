5e659a3e72e58ab0a5cb322f2ad38d546cba4ae2
Cristina Gonzales <2024161902@ub.edu.bz>
2025-09-30 10:33:30 -0600

    In order traversal

---
+++ ex.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct BinaryTreeNode {
+    string data;
+    BinaryTreeNode* left;
+    BinaryTreeNode* right;
+
+    BinaryTreeNode(const string& value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+void inorder(BinaryTreeNode* root);
+
+
+int main() {
+
+    return 0;
+}
+
+void inorder(BinaryTreeNode* root) {
+    if (root == nullptr){
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
